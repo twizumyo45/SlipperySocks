b84c287398c7a598dd99bbe11a7611ddaef86fe3
Roger <rlau4545@gmail.com>
2015-04-04 01:14:26 -0700

    added background

--- SlipperySocks/src/resource.js
+++ SlipperySocks/src/resource.js
@@ -1,7 +1,9 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
     CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png"
+    CloseSelected_png : "res/CloseSelected.png",
+    Koala_jpg : "res/Koala.jpg",
+    bg_jpg : "res/bg.jpg"
 };
 
 var g_resources = [];
