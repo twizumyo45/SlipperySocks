e72c84c913fa071e0419d511370396494119ec18
Noah Stier <noah.stier@gmail.com>
2015-04-04 02:01:22 -0700

    remove kid slide-in

--- SlipperySocks/src/AnimationLayer.js
+++ SlipperySocks/src/AnimationLayer.js
@@ -11,9 +11,6 @@ var AnimationLayer = cc.Layer.extend({
         var spriteCharacter = new cc.Sprite(res.stationary_kid_png);
         spriteCharacter.attr({x: 80, y: 85});
 
-        //create the move action
-        var actionTo = new cc.MoveTo(2, cc.p(300, 85));
-        spriteCharacter.runAction(new cc.Sequence(actionTo));
         this.addChild(spriteCharacter);
     }
 });
