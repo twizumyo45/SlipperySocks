abb63bcd52f17675f2b9262162546ea4df52c72d
Noah Stier <noah.stier@gmail.com>
2015-04-04 01:31:28 -0700

    remove old resource references

--- SlipperySocks/src/resource.js
+++ SlipperySocks/src/resource.js
@@ -1,7 +1,4 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-    CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png",
     Koala_jpg : "res/Koala.jpg",
     bg_jpg : "res/bg.jpg",
     stationary_kid_png: 'res/stationary_kid.png',
