25669af70c5cf66a371c16789dbc0c378fa4d28f
Noah Stier <noah.stier@gmail.com>
2015-04-04 01:52:53 -0700

    inject chipmunk

--- SlipperySocks/src/PlayScene.js
+++ SlipperySocks/src/PlayScene.js
@@ -4,5 +4,16 @@ var PlayScene = cc.Scene.extend({
         //add three layer in the right order
         this.addChild(new BackgroundLayer());
         this.addChild(new AnimationLayer());
+    },
+    space: null,
+    initPhysics: function() {
+        //1. new space object 
+        this.space = new cp.Space();
+        //2. setup the  Gravity
+        this.space.gravity = cp.v(0, 0);
+    },
+    update:function (dt) {
+        // chipmunk step
+        this.space.step(dt);
     }
 });
