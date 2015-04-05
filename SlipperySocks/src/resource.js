9a27c46fe4f1016b189f7641119de4a7acdbbbbd
Noah Stier <noah.stier@gmail.com>
2015-04-04 18:17:36 -0700

    new play button

--- SlipperySocks/src/resource.js
+++ SlipperySocks/src/resource.js
@@ -2,7 +2,7 @@ var res = {
     Koala_jpg : "res/Koala.jpg",
     bg_jpg : "res/bg.jpg",
     stationary_kid_png: 'res/stationary_kid.png',
-    start_button_blue_jpg: 'res/start_button_blue.jpg',
+    start_button_blue_jpg: 'res/play_button_small.png', // lol
     candy_png: 'res/candy.png',
     running_kid_png: 'res/texturePacker/running_kid.png',
     running_kid_plist: 'res/texturePacker/running_kid.plist',
