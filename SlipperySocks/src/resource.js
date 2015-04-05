2e9c58bc19f0bc30f8b861079dfdac4ee8217361
Noah Stier <noah.stier@gmail.com>
2015-04-05 08:43:54 -0700

    ghost resources

--- SlipperySocks/src/resource.js
+++ SlipperySocks/src/resource.js
@@ -7,7 +7,9 @@ var res = {
     running_kid_png: 'res/texturePacker/running_kid.png',
     running_kid_plist: 'res/texturePacker/running_kid.plist',
     wall_png: 'res/wall.png',
-    wall2_png: 'res/wall2.png'
+    wall2_png: 'res/wall2.png',
+    ghost_png: 'res/ghosts/ghost.png',
+    ghost_plist: 'res/ghosts/ghost.plist'
 };
 
 var g_resources = [];
